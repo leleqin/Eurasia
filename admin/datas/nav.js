0418fc9e6843f65d6ee53e6deabe5e356fbb8bde
leleqin <602568224@qq.com>
2018-04-27 20:12:33 +0800

    前后台

---
+++ admin/datas/nav.js
@@ -0,0 +1,72 @@
+var navs = [{
+	"title": "系统管理",
+	"icon": "fa-cubes",
+	"spread": true,
+	"children": [{
+		"title": "管理员管理",
+		"icon": "&#xe613;",
+		"href": "begtable.html"
+        }, {
+		"title": "网站配置",
+		"icon": "&#xe641;",
+		"href": "button.html"
+	}, {
+		"title": "首页轮播图",
+		"icon": "&#xe63c;",
+		"href": "form.html"
+	}, {
+		"title": "文件上传管理",
+		"icon": "&#xe63c;",
+		"href": "table.html"
+	}]
+},{
+	"title": "中心介绍",
+	"icon": "&#x1002;",
+	"spread": false,
+	"children": [{
+        "title": "新增分类",
+        "icon": "&#xe61f;",
+        "href": "add.html"
+    },{
+        "title": "栏目管理",
+        "icon": "&#xe630;",
+        "href": "manager.html"
+    }]
+}, {
+	"title": "地址本",
+	"icon": "fa-address-book",
+	"href": "",
+	"spread": false,
+	"children": [{
+		"title": "Github",
+		"icon": "fa-github",
+		"href": "https://www.github.com/"
+	}, {
+		"title": "QQ",
+		"icon": "fa-qq",
+		"href": "http://www.qq.com/"
+	}, {
+		"title": "Fly社区",
+		"icon": "&#xe609;",
+		"href": "http://fly.layui.com/"
+	}, {
+		"title": "新浪微博",
+		"icon": "fa-weibo",
+		"href": "http://weibo.com/"
+	}]
+}, {
+	"title": "这是一级导航",
+	"icon": "fa-stop-circle",
+	"href": "https://www.baidu.com",
+	"spread": false
+}, {
+	"title": "其他",
+	"icon": "fa-stop-circle",
+	"href": "#",
+	"spread": false,
+	"children": [{
+		"title": "子窗体中打开选项卡",
+		"icon": "fa-github",
+		"href": "cop.html"
+	}]
+}];
