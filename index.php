a4784c05f65d6000411ba7addace7df7d6f29243
leleqin <602568224@qq.com>
2018-04-27 20:19:40 +0800

    test

--- index.php
+++ index.php
@@ -304,7 +304,6 @@
 								<li><a href="#"><i class="fa fa-twitter"></i></a></li>
 								<li><a href="#"><i class="fa fa-google-plus"></i></a></li>
 								<li><a href="#"><i class="fa fa-linkedin"></i></a></li>
-                                
 							</ul>
 						</div>
 					</div>
